a7fd6a3749a4e0d4887e10ab4fe0cebb7bdd4dbf
v1ctu <victorhugochimenton@icloud.com>
2020-11-01 21:47:45 -0300

    Added hikaricp

--- src/main/java/com/github/v1ctu/dao/impl/UserDao.java
+++ src/main/java/com/github/v1ctu/dao/impl/UserDao.java
@@ -9,21 +9,25 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
 
 @AllArgsConstructor
-public class UserDao implements Dao<UUID, UserEntity> {
+public class UserDao implements Dao<String, UserEntity> {
 
     private static final String REPLACE_STATEMENT = "REPLACE INTO accounts VALUES (?, ?, ?);";
-    private static final String FIND_STATEMENT = "SELECT * FROM accounts WHERE uuid = ? LIMIT 1;";
-    private static final String DELETE_STATEMENT = "DELETE FROM accounts WHERE uuid = ?;";
+    private static final String FIND_STATEMENT = "SELECT * FROM accounts WHERE UPPER(name) = UPPER(?) LIMIT 1;";
+    private static final String FIND_TOP_STATEMENT = "SELECT * FROM accounts ORDER BY cash DESC LIMIT 5;";
+    private static final String DELETE_STATEMENT = "DELETE FROM accounts WHERE name = ?;";
 
     private final CashPlugin plugin;
 
     @Override
     public void replace(UserEntity value) {
-        try (Connection connection = plugin.getMySql().getConnection()) {
-            PreparedStatement statement = connection.prepareStatement(REPLACE_STATEMENT);
+        try (Connection connection = plugin.getMySql().getConnection();
+             PreparedStatement statement = connection.prepareStatement(REPLACE_STATEMENT)) {
+
             statement.setString(1, value.getUuid().toString());
             statement.setString(2, value.getName());
             statement.setDouble(3, value.getQuantity());
@@ -35,8 +39,9 @@ public class UserDao implements Dao<UUID, UserEntity> {
 
     @Override
     public void delete(UserEntity value) {
-        try (Connection connection = plugin.getMySql().getConnection()) {
-            PreparedStatement statement = connection.prepareStatement(DELETE_STATEMENT);
+        try (Connection connection = plugin.getMySql().getConnection();
+             PreparedStatement statement = connection.prepareStatement(DELETE_STATEMENT)) {
+
             statement.setString(1, value.getUuid().toString());
             statement.executeUpdate();
         } catch (Exception exception) {
@@ -45,12 +50,13 @@ public class UserDao implements Dao<UUID, UserEntity> {
     }
 
     @Override
-    public UserEntity find(UUID key) {
-        try (Connection connection = plugin.getMySql().getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement(FIND_STATEMENT);
+    public UserEntity find(String key) {
+        try (Connection connection = plugin.getMySql().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(FIND_STATEMENT)) {
+
             preparedStatement.setString(1, key.toString());
             ResultSet resultSet = preparedStatement.executeQuery();
-            if(!resultSet.next()) return null;
+            if (!resultSet.next()) return null;
             return UserEntity.builder()
                     .uuid(UUID.fromString(resultSet.getString("uuid")))
                     .name(resultSet.getString("name"))
@@ -64,6 +70,24 @@ public class UserDao implements Dao<UUID, UserEntity> {
 
     @Override
     public Collection<UserEntity> find() {
+        try (Connection connection = plugin.getMySql().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(FIND_TOP_STATEMENT)) {
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<UserEntity> list = new LinkedList<>();
+
+            while (resultSet.next()) {
+                list.add(UserEntity.builder()
+                        .uuid(UUID.fromString(resultSet.getString("uuid")))
+                        .name(resultSet.getString("name"))
+                        .quantity(resultSet.getDouble("cash"))
+                        .build());
+            }
+            return list;
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
         return null;
     }
 }
--- src/main/java/com/github/v1ctu/storage/MySQL.java
+++ src/main/java/com/github/v1ctu/storage/MySQL.java
@@ -1,7 +1,9 @@
 package com.github.v1ctu.storage;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -13,7 +15,7 @@ public class MySQL {
     private final String database;
     private final int port;
 
-    private Connection connection;
+    private HikariDataSource hikariDataSource;
 
     public MySQL(String host, String user, String password, String database, int port) {
         this.host = host;
@@ -25,37 +27,37 @@ public class MySQL {
 
     public void openConnection() {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            connect();
+            HikariConfig hikariConfig = new HikariConfig();
+            hikariConfig.setJdbcUrl("jdbc:mysql://" + host + ":" + port + "/" + database);
+            hikariConfig.setDriverClassName("com.mysql.jdbc.Driver");
+            hikariConfig.setUsername(user);
+            hikariConfig.setPassword(password);
+            hikariConfig.setMaximumPoolSize(8);
+            hikariConfig.setMinimumIdle(2);
+            hikariConfig.addDataSourceProperty("cachePrepStmts", "true");
+            hikariConfig.addDataSourceProperty("prepStmtCacheSize", "250");
+            hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+            hikariDataSource = new HikariDataSource(hikariConfig);
+
             createTable();
         } catch (Exception exception) {
             exception.printStackTrace();
         }
     }
 
-    public Connection getConnection() {
-        try {
-            if (connection == null || connection.isClosed())
-                connect();
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-        return connection;
-    }
-
-    private void connect() throws SQLException {
-        connection = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database, user, password);
+    public Connection getConnection() throws SQLException {
+        return hikariDataSource.getConnection();
     }
 
     private void createTable() {
-        try (Statement statement = connection.createStatement()) {
+        try (Statement statement = getConnection().createStatement()) {
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS accounts (" +
                     "uuid VARCHAR(36) NOT NULL, " +
                     "name VARCHAR(16) NOT NULL, " +
                     "cash DOUBLE NOT NULL," +
                     "primary key (uuid)" +
                     ")");
-
         } catch (Exception exception) {
             exception.printStackTrace();
         }
